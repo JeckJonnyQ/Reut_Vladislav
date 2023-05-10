6ae11d7963727710117d8aa431a6225d1952c151
JeckJonny <JeckandJonny@Gmail.com>
2023-05-10 14:25:51 +0300

    Проект Экз

--- Ex-Maket.html/JS/slayder.js
+++ Ex-Maket.html/JS/slayder.js
@@ -15,16 +15,43 @@ function nextSlide() {
     }
 }
 
-$('.slide-nav-btn').click(function() {
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('#viewport').width() * (slideCount - 1);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow - 2);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
+
+
+$('#next-btn').click(function () {
+    nextSlide();
+});
+
+
+$('#prev-btn').click(function () {
+    prevSlide();
+});
+
+
+
+$('.slide-nav-btn').click(function () {
     navBtnId = $(this).index();
 
     if (navBtnId + 1 != slideNow) {
         translateWidth = -$('#viewport').width() * (navBtnId);
         $('#slidewrapper').css({
             'transform': 'translate(' + translateWidth + 'px, 0)',
-            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
         });
         slideNow = navBtnId + 1;
     }
 });
+
